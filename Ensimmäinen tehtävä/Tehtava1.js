bbe737500d15c3714138f00edca2b4a339b3fa13
JMKangas <80038481+JMKangas@users.noreply.github.com>
2022-01-24 21:43:48 +0200

    ensimmäinen tehtävä

---
+++ Ensimmäinen tehtävä/Tehtava1.js
@@ -0,0 +1,25 @@
+
+var tervehdys = "Heipä hei! Haluatko kuulla sadun?";
+
+    alert(tervehdys);
+
+var ensimmainen = "Olipa kerran...";
+
+    alert(ensimmainen);
+
+    ensimmainen = "Suomessa...";
+
+    alert(ensimmainen);
+
+var toinen = "Hyvä hallitus!";
+
+    alert(toinen);
+
+var kolmas = "mutta se olikin vain satu.";
+
+    alert(kolmas);
+
+    const y = "sen pituinen se";
+
+    alert(y);
+    
