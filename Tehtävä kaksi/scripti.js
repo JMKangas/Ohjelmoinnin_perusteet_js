6c4c138c179bd107843baa3a38fa2d93ff37462f
JMKangas <80038481+JMKangas@users.noreply.github.com>
2022-01-26 00:45:23 +0200

    tehtavia

---
+++ Tehtävä kaksi/scripti.js
@@ -0,0 +1,29 @@
+//Haaste 1 äärimmainen ∞
+
+var eka = 10 / 0;
+
+console.log(eka);
+
+//haaste 2 potenssit
+
+var toka = 2;
+
+console.log(toka ** 3);
+
+var kolmas = 3 ** 3;
+
+console.log(kolmas);
+
+//haaste 3 modulus operaattori
+
+var neljas = 5 % 2;
+
+console.log(neljas);
+
+var viides = 10 % 5;
+
+console.log(viides);
+
+var kuudes = -42 % -10;
+
+console.log(kuudes);
---
+++ Tehtävä kolme/javascripti.js
@@ -0,0 +1,14 @@
+let a, b, c, d;
+
+a = 5;
+b = 6;
+c = a + b;
+
+// Tehtävät
+d = 1.9091;
+console.log(a + b + c + d);
+c = (a + b) * d;
+c = Math.round(c);
+console.log(c);
+
+// En ollu ihan varma mitä tässä tehtävässä haluttiin, mutta mielestäni olen ainakin likellä. :)
